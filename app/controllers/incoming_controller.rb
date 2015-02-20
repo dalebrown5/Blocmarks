65503896fbb94c21c3c6193c7ab101f1d56d15fa
Dale <dalebrown5@gmail.com>
2015-02-20 16:28:11 -0700

    started to configure the incoming controler

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -7,23 +7,27 @@ class IncomingController < ApplicationController
   end
 
   def create
+
+    puts ">>>>>>>>>> #{params.inspect}"
+    
+    @user = User.find_by(email: params[:sender]) 
+    @topic = Topic.find_by(email: params[:subject])
+    @url = params["body-plain"]
+
+    # if user == nil?
+    # end
+
+    # if topic == nil?
+    # end
+
     # You put the message-splitting and business
     # magic here.
     # Find the user by using params[:sender]
-    puts ">>>>>>>>>> #{params.inspect}"
-    # @user = User.find(params[:sender])
     # Find the topic by using params[:subject]
-    # @topic = Topic.find(params[:subject])
     # Assign the url to a variable after retreiving it from params["body-plain"]
-    # @body = params["body-plain"]
     # Check if user is nil, if so, create and save a new user
-    # if user == nil?
-    # end
     # Check if the topic is nil, if so, create and save a new topic
-    # if topic == nil?
-    # end
     # Now that we're sure we have a valid user and topic, build and save a new bookmark
-
     # Assuming all went well. 
 
     head 200
