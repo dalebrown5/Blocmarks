03a7af63597f9d9826c2f1e49c5831784853c897
Dale <dalebrown5@gmail.com>
2015-02-20 16:50:30 -0700

    Addded Topics and Bookmarks controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update, :show, :index]
 
+  resources :topics do
+    resources :bookmarks, only: [:show, :create, :update]
+  end
+
   post :incoming, to: 'incoming#create'
   
   get 'welcome/index'
